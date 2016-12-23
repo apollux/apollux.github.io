94891d856d34c61ae8dbe992836466aa1e05eada
Andre Kwakernaak <andre.kwakernaak@gmail.com>
2016-12-23 19:07:55 +0100

    Fixed serve-drafts.sh script

--- serve-drafts.sh
+++ serve-drafts.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo docker run -v "$PWD":/usr/src/app -p "4000:4000" starefossen/github-pages
+sudo docker run -v "$PWD":/usr/src/app -p "4000:4000" starefossen/github-pages jekyll serve -d /_site --drafts --watch --force_polling -H 0.0.0.0 -P 4000
