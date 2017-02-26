3a25146c66542df44b1dcfd5da4eed0ea00dffca
Andre Kwakernaak <andre.kwakernaak@gmail.com>
2017-02-26 17:47:35 +0100

    Remove docker images once done.

--- serve.sh
+++ serve.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo docker run -v "$PWD":/usr/src/app -p "4000:4000" starefossen/github-pages
+sudo docker run --rm -v "$PWD":/usr/src/app -p "4000:4000" starefossen/github-pages
