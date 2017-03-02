cf7c6df448946161089d6416a557fb7741266d92
Andre Kwakernaak <andre.kwakernaak@gmail.com>
2017-03-02 19:45:51 +0100

    No need for sudo.

--- serve-drafts.sh
+++ serve-drafts.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo docker run --rm -v "$PWD":/usr/src/app -p "4000:4000" starefossen/github-pages jekyll serve -d /_site --drafts --watch --force_polling -H 0.0.0.0 -P 4000
+docker run --rm -v "$PWD":/usr/src/app -p "4000:4000" starefossen/github-pages jekyll serve -d /_site --drafts --watch --force_polling -H 0.0.0.0 -P 4000
--- serve.sh
+++ serve.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo docker run --rm -v "$PWD":/usr/src/app -p "4000:4000" starefossen/github-pages
+docker run --rm -v "$PWD":/usr/src/app -p "4000:4000" starefossen/github-pages
